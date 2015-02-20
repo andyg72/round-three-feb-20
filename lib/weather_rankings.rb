8792f2858b7f274bb86b32ee54cb42ea961abc51
Andy <andygnewman@gmail.com>
2015-02-20 14:16:03 +0000

    final commit

---
+++ lib/weather_rankings.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+
+def temp(city)
+  uri = URI("http://api.openweathermap.org/data/2.5/weather?q=#{city}")
+  data = Net::HTTP.get(uri)
+  JSON.parse(data)['main']['temp']
+end
+
+def array_temp(array)
+  hash = Hash.new(0)
+  array.each { |city| hash[city.to_sym] = temp(city)}
+  hash
+end
+
+def rank_by_temp(array)
+  hash = array_temp(array)
+  answer = hash.sort_by { |key, value| value}
+  answer.keys.reverse
+end
--- spec/weather_rankings_spec.rb
+++ spec/weather_rankings_spec.rb
@@ -1,1 +1,17 @@
 require 'weather_rankings'
+
+describe 'temperature rankings' do
+
+  it 'should get the temperature for London' do
+    expect(temp('London')).to eq(280.05)
+  end
+
+  it 'should get the temperature for an array of cities' do
+    expect(array_temp(['London'])).to eq({London: 280.05})
+  end
+
+  it 'should sort the cities in rank of temp' do
+    expect(rank_by_temp(['Paris', 'London', 'Seoul', 'Manila'])).to eq(['Manila', 'Paris', 'London', 'Seoul'])
+  end
+
+end
